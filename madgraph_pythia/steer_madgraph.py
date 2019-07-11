83396f4c6125c3a1367559f04e16bc5ce44809bc
Alex Schuy <alexjschuy@gmail.com>
2019-07-11 05:18:54 -0700

    Changes ufo to tar file.

--- madgraph_pythia/steer_madgraph.py
+++ madgraph_pythia/steer_madgraph.py
@@ -17,11 +17,15 @@ def edit_proc_card(proc_card_path, param_card_path, run_card_path, n_events):
             f.write('set nevents {}\n'.format(n_events))
         f.write('0\n')
 
-def run_madgraph(proc_card_path, output_path, ufo_path, param_card_path=None, run_card_path=None, n_events=None):
-    if ufo_path is not None:
-        ufo_path = ufo_path.rstrip('/')
-        ufo_dir = os.path.basename(ufo_path)
-        shutil.copytree(ufo_path, 'madgraph/models/{}'.format(ufo_dir))
+def run_madgraph(proc_card_path, output_path, ufotar, param_card_path=None, run_card_path=None, n_events=None):
+    if ufotar is not None:
+        if ufotar.contains('/'):
+            ufo_name, ufo_ext = os.path.splitext(os.path.basename(ufotar))
+        else:
+            ufo_name, ufo_ext = os.path.splitext(ufotar)
+        assert ufo_ext == '.tar', 'ufo must be a tar file!'
+        subprocess.call(['tar', '-xvf', ufotar])
+        shutil.copytree(ufo_name, 'madgraph/models/{}'.format(ufo_name))
     proc_card_copy_path = os.path.join(os.getcwd(), 'proc_card.dat')
     shutil.copyfile(proc_card_path, proc_card_copy_path)
     edit_proc_card(proc_card_copy_path, param_card_path, run_card_path, n_events)
@@ -35,13 +39,13 @@ def main():
     parser = argparse.ArgumentParser(description='Run madgraph+pythia.')
     parser.add_argument('proc_card', help='path to proc_card.')
     parser.add_argument('output', help='path for output hepmc.')
-    parser.add_argument('--ufo', help='path to UFO model directory.')
+    parser.add_argument('--ufotar', help='path to UFO tar file.')
     parser.add_argument('--param_card', help='path to param_card. Leave unspecified or pass "default" to use the default param card for the model.')
     parser.add_argument('--run_card', help='path to run_card. Leave unspecified or pass "default" to use the default run card for the model.')
     parser.add_argument('--n_events', '-n', type=int, help='Number of events.')
 
     args = parser.parse_args()
-    run_madgraph(args.proc_card, args.output, args.ufo, args.param_card, args.run_card, args.n_events)
+    run_madgraph(args.proc_card, args.output, args.ufotar, args.param_card, args.run_card, args.n_events)
 
 
 if __name__ == '__main__':
