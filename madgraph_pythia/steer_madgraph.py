f5dfc7a2483881a04644a5b5f43abb7fc0f861aa
Alex Schuy <alexjschuy@gmail.com>
2019-07-13 11:56:01 +0200

    Fixes typo.

--- madgraph_pythia/steer_madgraph.py
+++ madgraph_pythia/steer_madgraph.py
@@ -32,7 +32,7 @@ def run_madgraph(proc_card_path, output_path, ufotar, param_card_path=None, run_
     edit_proc_card(proc_card_copy_path, param_card_path,
                    run_card_path, n_events)
     subprocess.call(['/code/madgraph/bin/mg5_aMC', proc_card_copy_path])
-    run_dir = os.path.join(os.cwd(), 'PROC_sm_0', 'Events', 'run_1')
+    run_dir = os.path.join(os.getcwd(), 'PROC_sm_0', 'Events', 'run_1')
     subprocess.call(['gunzip', os.path.join(
         run_dir, 'tag_1_pythia8_events.hepmc.gz')])
     subprocess.call(['sudo', 'cp', os.path.join(
